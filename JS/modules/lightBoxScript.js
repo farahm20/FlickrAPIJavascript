940da6306966b48d51e7b742ac254433325a6676
jensengbg-Farah-Mahboob <45151545+farahm20@users.noreply.github.com>
2020-03-25 09:42:04 +0100

    Before creating the modules.

---
+++ JS/modules/lightBoxScript.js
@@ -0,0 +1,43 @@
+
+ const galleryItems = document.querySelector(".gallerySection").children;
+ const lightBoxContainer = document.querySelector(".lightBox");
+ const lightBoxImage = document.querySelector(".lightBoxImage");
+ let index;
+ let imageSrc;
+ 
+//******LightBOX event listener added to all the images recived from flickr api
+function addClickToImages(){
+    for(let i = 0; i < galleryItems.length ; i++){
+        console.log("For loop: all gallery Items " + galleryItems[i]);
+        galleryItems[i].addEventListener("click", function(){
+            console.log(i);
+            index = i;
+
+            selectImageforLightBox();
+            lightBox();
+        })
+    }
+}
+
+//*******LightBOX will close if clicked outside the picture or on x
+lightBoxContainer.addEventListener("click", function(event){
+    if(event.target !== lightBoxImage){
+        lightBox();
+    }
+})
+
+//*******LightBOX: selects the picture to show in lightbox.
+function lightBox(){
+    console.log("In teh lightbox function" + lightBoxContainer);
+    lightBoxContainer.classList.toggle("open");
+}
+
+//*******LightBOX: this will select images to appear in the lightbox. 
+function selectImageforLightBox () {
+    console.log(galleryItems[index])
+    imageSrc = galleryItems[index].querySelector("img").getAttribute("src");
+    console.log("In the selectImageforLightBox function " + imageSrc);
+    lightBoxImage.src = imageSrc;
+}
+
+export default {addClickToImages, addEventListener, lightBox, selectImageforLightBox};
--- JS/script.js
+++ JS/script.js
@@ -1,55 +1,35 @@
-const searchButton = document.querySelector('#search');
-const imagesCollection = document.querySelector('.main');
-const galleryItems = document.querySelector(".gallerySection").children;
-//console.log("The galelrySection Div: " + galleryItems);
+import {addClickToImages, addEventListener, lightBox, selectImageforLightBox} from 'lightBoxScript.js';
 
-const lightBoxContainer = document.querySelector(".lightBox");
-const lightBoxImage = document.querySelector(".lightBoxImage");
-let index;
-let imageSrc;
-refreshPage() ;
-function addClickToImages(){
-    for(let i = 0; i < galleryItems.length ; i++){
-        console.log("For loop: all gallery Items " + galleryItems[i]);
-        galleryItems[i].addEventListener("click", function(){
-            console.log(i);
-            index = i;
+const searchButton = document.querySelector('#search'); //getting the search button
 
-            changeImage();
-            lightBox();
-        })
-    }
-}
-lightBoxContainer.addEventListener("click", function(event){
-    if(event.target !== lightBoxImage){
-        lightBox();
-    }
-})
 
-function lightBox(){
-    console.log("In teh lightbox function" + lightBoxContainer);
-    lightBoxContainer.classList.toggle("open");
-}
+refreshPage() ; //refreshes the page in middle of a search output
 
-function changeImage () {
-    console.log(galleryItems[index])
-    imageSrc = galleryItems[index].querySelector("img").getAttribute("src");
-    console.log("In the changeImage function " + imageSrc);
-    lightBoxImage.src = imageSrc;
-}
+//*****the refresh page button function
+function refreshPage() {
+        let refrehButton = document.getElementById('refresh');
+        refrehButton.addEventListener('click', function() {
+        setTimeout(location.reload(true),t)
+    });  
+ }
 
+
+//*******appends img src in html for every image recieved from Flickr Api
 function showImagesInHtml(data) {
-    console.log("In the show images in HTML function");
+//  console.log("In the show images in HTML function");
     let sendAsOutput = document.getElementById("gallery");
+    sendAsOutput.innerHTML = '';
+   
     data.photos.photo.forEach(element => {
         let galleryDiv = document.createElement('div')
-     galleryDiv.innerHTML = (`<img src="https://farm${element.farm}.staticflickr.com/${element.server}/${element.id}_${element.secret}.jpg"/>`);
+        galleryDiv.innerHTML = (`<img src="https://farm${element.farm}.staticflickr.com/${element.server}/${element.id}_${element.secret}.jpg"/>`);
         gallery.appendChild(galleryDiv); 
     });
 
     addClickToImages();
 }
 
+//*******async funtion that connects with flickr API. Function takes images title search and no of images.
 async function searchTheApi(imageName, imageNum)
 {
     let tagName = imageName;
@@ -62,36 +42,24 @@ async function searchTheApi(imageName, imageNum)
     const FORMAT = '&format=json&nojsoncallback=1'
     const SORT_RELEVANCE = '&sort=relevance'
     const PER_PAGE = '&per_page=' + noOfImages;
-    console.log("The image number goes in url: " + noOfImages);
+//  console.log("The image number goes in url: " + noOfImages);
     const url = `${BASE_URL}${METHOD}${KEY}${SORT_RELEVANCE}${FORMAT}&text=${tagName}${PER_PAGE}&page=${page}`;
 
-//here will come the title and num of photos user wants
-//    console.log("The Url " + url);
-
+//console.log("The Url " + url);
     try {
         let response = await fetch(url);
         let data = await response.json();
    //   console.log("The data " + data);
         showImagesInHtml(data);
-   //     addInHtml(data);
      } catch(error) {
          console.log('ERROR IN FTECH: ', error);
      }
-
-    console.log(" " + imageName + " " + imageNum);
+//    console.log(" " + imageName + " " + imageNum);
 }
 
+//*******buttons that take values from the search bar and send those values to function searchTheAPi
 searchButton.addEventListener('click', function() {
     let imageName = document.querySelector('#searchBarId').value;
     let imageNum = document.querySelector('#numberBarId').value;
     searchTheApi(imageName, imageNum);
 });
-
-function refreshPage() {
-    let refrehButton = document.getElementById('refresh');
-    refrehButton.addEventListener('click', function() {
-         setTimeout(location.reload(true),t)
-    });  
- }
-
-
